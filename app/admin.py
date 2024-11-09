7c5716d6c19fe152fc3fe153ce585ab6db7960ab
GabrielSilva <gabrielmsilva4@ua.pt>
2024-11-09 01:34:22 +0000

    add models to admin page

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from app.models import Profile, Category, Service, Booking
+from app.models import *
 
 admin.site.register(Profile)
 admin.site.register(Category)
 admin.site.register(Service)
 admin.site.register(Booking)
+admin.site.register(Review)
+admin.site.register(Chat)
+admin.site.register(Message)
+admin.site.register(Provider)
